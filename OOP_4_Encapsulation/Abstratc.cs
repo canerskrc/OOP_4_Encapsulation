839b3ec2a808988e9ba6dfac6e7c91e93f078c38
canersekerci <62672535+canerskrc@users.noreply.github.com>
2023-08-23 14:32:01 +0300

    Proje dosyası ekle.

---
+++ OOP_4_Encapsulation/Abstratc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Abstract
+{
+
+    // abstract class
+    abstract class Sekiller
+    {
+
+        // abstract metot
+        public abstract int alan();
+    }
+
+    // Sekiller sınıfından kare sınıfı kalıtım oluşturma
+    class kare : Sekiller
+    {
+
+        // veri oluşturma
+        private int kenar;
+
+        // kare sınıfına ait metot
+        public kare(int x = 0)
+        {
+            kenar = x;
+        }
+
+        // override ile Sekiller sınıfının 
+        // soyutlama yöntemi geçersiz hale getirilir.
+        public override int alan()
+        {
+            Console.Write("Karenin alani : ");
+            return (kenar * kenar);
+        }
+    }
+
+    // Driver Class
+    class FKA
+    {
+
+        // Main Metot
+        static void Main(string[] args)
+        {
+
+            // Kare sınıfındaki veriye
+            // Sekiller sınıfına ait
+            // referans verme
+             Sekiller sh = new kare();
+
+            // MEtot Çağırma
+            double result = sh.alan();
+
+            Console.WriteLine("{0}", result);
+
+        }
+    }
+}
---
+++ OOP_4_Encapsulation/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+using System;
+
+public class Kapsulleme
+{
+
+    //Bu kısımdaki verilere yalnızca Kapsulleme 
+    // isimli temel(ana) sınıf tarafından erişilir.
+    private String Isım;
+    private int Yas;
+
+    // Erişimcileri kullanarak (get,set)
+    // Kisi adı değeri alınır(get) ve ayarlanır(set).
+    public String KisiAdi
+    {
+
+        get { return Isım; }
+
+        set { Isım = value; }
+    }
+
+    // Yukarıda olduğu gibi kisi yaşı 
+    // değeri alınır ve ayarlanır.
+    public int KisiYasi
+    {
+
+        get { return Yas; }
+
+        set { Yas = value; }
+    }
+}
+
+// Driver Class
+class FKA
+{
+
+    // Main Method
+    static public void Main()
+    {
+
+        // Nesne oluşturulur
+        Kapsulleme obj = new Kapsulleme();
+
+        // Kisi adı özelliğinin set erişimcisi
+        // çağırılır. ve Kisi adına burada girilen
+        // değer atanır.
+        obj.KisiAdi = "Caner";
+        
+
+        
+        
+
+        // Yukarıda olduğu gibi kisi yasına 
+        // set erişimcisi ile yaş değeri atanır
+        obj.KisiYasi = 27;
+
+        // Değişkenler ekranda gösterilir.
+        Console.WriteLine(" Isim : " + obj.KisiAdi);
+        Console.WriteLine(" Yaş : " + obj.KisiYasi);
+    }
+}
+*/
